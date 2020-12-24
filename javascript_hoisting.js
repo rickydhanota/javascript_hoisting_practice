8f60f36940ec595791796d26d68ce58833fd8b1b
Ricky <rickdhanota@gmail.com>
2020-12-23 21:35:55 -0800

    practice problems 3 and 4

--- javascript_hoisting.js
+++ javascript_hoisting.js
@@ -7,9 +7,30 @@ console.log(example);
 // console.log(example); // logs undefined
 // example = "I'm the example!";
 
+
+//problem 2
 function test(){
     var needle = 'magnet';
     console.log(needle);
 }
 var needle = 'haystack';
 test();
+
+//problem 3
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+}
+brendan = 'super cool';
+console.log(brendan);
+
+//problem 4
+
+function eat(){
+    food = 'half-chicken';
+    console.log(food);
+    var food = 'gone';
+}
+var food = 'chicken';
+eat();
+console.log(food);
