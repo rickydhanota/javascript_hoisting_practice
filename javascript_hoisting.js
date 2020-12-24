a5c4c6a3e9d152a32314df1b03f2012fcde75b11
Ricky <rickdhanota@gmail.com>
2020-12-23 17:10:37 -0800

    initial commit

---
+++ javascript_hoisting.js
@@ -0,0 +1,7 @@
+// GIVEN
+console.log(example);
+var example = "I'm the example!";
+//AFTER HOISTING BY THE INTERPRETER
+// var example;
+// console.log(example); // logs undefined
+// example = "I'm the example!";
