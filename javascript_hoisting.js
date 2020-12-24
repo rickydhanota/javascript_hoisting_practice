53bc12d74349002b34da063e18c05d281cdb1f70
Ricky <rickdhanota@gmail.com>
2020-12-23 21:29:36 -0800

    practice problem 2

--- javascript_hoisting.js
+++ javascript_hoisting.js
@@ -1,7 +1,15 @@
 // GIVEN
-console.log(example);
 var example = "I'm the example!";
+console.log(example);
+
 //AFTER HOISTING BY THE INTERPRETER
 // var example;
 // console.log(example); // logs undefined
 // example = "I'm the example!";
+
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+var needle = 'haystack';
+test();
