efd1685ef390417d37cea14e5672aea00164b89d
Ricky <rickdhanota@gmail.com>
2020-12-23 21:45:41 -0800

    finished assignment

--- javascript_hoisting.js
+++ javascript_hoisting.js
@@ -9,41 +9,42 @@ console.log(example);
 
 
 //problem 2
+var needle = 'haystack';
+test();
 function test(){
     var needle = 'magnet';
     console.log(needle);
 }
-var needle = 'haystack';
-test();
 
 //problem 3
+var brendan = 'super cool';
 function print(){
     brendan = 'only okay';
     console.log(brendan);
 }
-brendan = 'super cool';
 console.log(brendan);
 
 //problem 4
-
+var food = 'chicken';
+console.log(food);
+eat();
 function eat(){
     food = 'half-chicken';
     console.log(food);
     var food = 'gone';
 }
-var food = 'chicken';
-eat();
-console.log(food);
 
 //problem 5
+console.log(food);
 var mean = function() {
     food = "chicken";
     console.log(food);
     var food = "fish";
     console.log(food);
 }
-console.log(food);
 mean();
+
+console.log(food);
 
 // var mean;
 // mean();
@@ -57,7 +58,9 @@ mean();
 // console.log(food);
 
 //problem 6
+console.log(genre);
 var genre = "disco";
+rewind();
 function rewind() {
     genre = "rock";
     console.log(genre);
@@ -65,4 +68,31 @@ function rewind() {
     console.log(genre);
 }
 console.log(genre);
-rewind();
+
+//problem 7
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
+}
+console.log(dojo);
+
+//problem 8
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
+}
